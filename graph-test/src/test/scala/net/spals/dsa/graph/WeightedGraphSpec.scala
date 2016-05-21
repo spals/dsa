5b7d5eca13b2d3becef1e20cd55ae0c5654f768b
Tim Kral <tim@yespath.com>
2016-05-21 15:07:17 -0700

    Add more bdd tests for weighted graph

--- graph-test/src/test/scala/net/spals/dsa/graph/WeightedGraphSpec.scala
+++ graph-test/src/test/scala/net/spals/dsa/graph/WeightedGraphSpec.scala
@@ -1,18 +1,54 @@
 package net.spals.dsa.graph
 
-import org.scalatest.{FunSpec, Matchers}
+import org.scalatest.{GivenWhenThen, FunSpec, Matchers}
 
 /**
   * BDD tests for [[WeightedGraph]]
   *
   * @author tkral
   */
-class WeightedGraphSpec extends FunSpec with Matchers {
+class WeightedGraphSpec extends FunSpec with GivenWhenThen with Matchers {
 
   describe("A weighted graph") {
-    it("should have an empty state") {
+    it("should have an empty state without vertices") {
+      Given("an empty graph")
       val emptyGraph = WeightedGraph.empty
+
+      Then("no vertices should be present")
       emptyGraph.vertices.size should be (0)
     }
+  }
+
+  it("should allow for vertices with edges") {
+    Given("a graph with only two vertices")
+    val wGraph = WeightedGraph.empty[String, Double].addVertex(Vertex("a")).addVertex(Vertex("b"))
+
+    When("an edge with weight 1.0 is added between them")
+    val newWGraph = wGraph.addEdge(Vertex("a"), Vertex("b"), 1.0d)
+
+    Then("an edge should exist between them")
+    newWGraph.hasEdge(Vertex("a"), Vertex("b")) should be (true)
+    val edge = newWGraph.getEdge(Vertex("a"), Vertex("b"))
+    edge should not be (None)
+
+    And("the edge has a weight of 1.0")
+    edge.get.weight should be (1.0d)
+
+    And("both vertices should still exist")
+    newWGraph.vertices.size should be (2)
+  }
+
+  it("should allow for vertices without edges") {
+    Given("a graph with two vertices and an edge")
+    val wGraph = WeightedGraph.empty.addEdge(Vertex("a"), Vertex("b"), 1.0d)
+
+    When("the edge is removed")
+    val newWGraph = wGraph.removeEdge(Vertex("a"), Vertex("b"))
+
+    Then("no edge exists between them")
+    newWGraph.hasEdge(Vertex("a"), Vertex("b")) should be (false)
+
+    And("both vertices should still exist")
+    newWGraph.vertices.size should be (2)
   }
 }
--- graph/src/main/scala/net/spals/dsa/graph/Graph.scala
+++ graph/src/main/scala/net/spals/dsa/graph/Graph.scala
@@ -6,9 +6,9 @@ package net.spals.dsa.graph
   * @author spags
   * @author tkral
   */
-case class Edge[T, W](v1: Vertex[T], v2: Vertex[T], w: W) {  }
+case class Edge[T, W](v1: Vertex[T], v2: Vertex[T], weight: W) {  }
 
-case class Vertex[T](v: T) {  }
+case class Vertex[T](label: T) {  }
 
 object WeightedGraph {
 
@@ -17,10 +17,10 @@ object WeightedGraph {
 
 case class WeightedGraph[T, W](vertices: Set[Vertex[T]], edgeFunc: (Vertex[T], Vertex[T]) => Option[W]) {
 
-  def addEdge(v1: Vertex[T], v2: Vertex[T], w: W): WeightedGraph[T, W] = {
+  def addEdge(v1: Vertex[T], v2: Vertex[T], weight: W): WeightedGraph[T, W] = {
     val addEdgeFunc: (Vertex[T], Vertex[T]) => Option[W] =
       (vx, vy) => (vx, vy) match {
-        case (vx, vy) if (vx == v1 && vy == v2) || (vx == v2 && vy == v1) => Option(w)
+        case (vx, vy) if (vx == v1 && vy == v2) || (vx == v2 && vy == v1) => Option(weight)
         case _ => edgeFunc.apply(vx, vy)
       }
     WeightedGraph(vertices ++ Set(v1, v2), addEdgeFunc)
@@ -30,24 +30,34 @@ case class WeightedGraph[T, W](vertices: Set[Vertex[T]], edgeFunc: (Vertex[T], V
     WeightedGraph(vertices + v, edgeFunc)
   }
 
-  def getEdge(v1: Vertex[T], v2: Vertex[T]): Option[W] = {
-    edgeFunc.apply(v1, v2)
+  def getEdge(v1: Vertex[T], v2: Vertex[T]): Option[Edge[T, W]] = {
+    edgeFunc.apply(v1, v2).map(Edge(v1, v2, _))
   }
 
   def getEdges(v1: Vertex[T]): Set[Edge[T, W]] = {
-    vertices.map(v2 => (v2, edgeFunc.apply(v1, v2)))
+    vertices.map(v2 => (v2, edgeFunc.apply(v1, v2) /*weight*/))
       .filter(_._2.isDefined)
       .map(tuple => new Edge[T, W](v1, tuple._1, tuple._2.get))
   }
 
   def getNeighbors(v1: Vertex[T]): Set[Vertex[T]] = {
-    vertices.map(v2 => (v2, edgeFunc.apply(v1, v2)))
+    vertices.map(v2 => (v2, edgeFunc.apply(v1, v2) /*weight*/))
       .filter(_._2.isDefined)
       .map(_._1)
   }
 
   def hasEdge(v1: Vertex[T], v2: Vertex[T]): Boolean = {
     edgeFunc.apply(v1, v2).isDefined
+  }
+
+  def removeEdge(v1: Vertex[T], v2: Vertex[T]): WeightedGraph[T, W] = {
+    val removeEdgeFunc: (Vertex[T], Vertex[T]) => Option[W] =
+      (vx, vy) => (vx, vy) match {
+        case (vx, vy) if (vx == v1 && vy == v2) || (vx == v2 && vy == v1) => None
+        case _ => edgeFunc.apply(vx, vy)
+      }
+
+    WeightedGraph(vertices, removeEdgeFunc)
   }
 
   def removeVertex(v: Vertex[T]): WeightedGraph[T, W] = {
